8cf4c7b790e1684d150284b6c150bb499c6f01ed
GithubPrashanth1 <prashanth.sachin847@gmail.com>
2022-02-08 23:05:13 +0000

    Second Commit

--- VSProject/Program.cs
+++ VSProject/Program.cs
@@ -7,6 +7,7 @@ namespace VSProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Prashanth!");
         }
     }
 }
