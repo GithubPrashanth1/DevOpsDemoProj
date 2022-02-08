03b098364eacf905914fc83e98631b95938adfbf
GithubPrashanth1 <prashanth.sachin847@gmail.com>
2022-02-08 22:56:41 +0000

    * .gitignore: First Commit
    * VSProject.sln:
    * Program.cs:
    * VSProject.csproj:

---
+++ VSProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
